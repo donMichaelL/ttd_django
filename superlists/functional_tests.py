fcf2eda72140ca318d16fcdc9eba5939019a4af9
Michael Loukeris <michael.loukeris@icloud.com>
2016-01-24 22:02:08 +0200

    Functional Test now checks header in homepage

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -10,9 +10,12 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	# page title and header
 	def test_check_title(self):
 		self.browser.get('http://localhost:8000')
 		self.assertIn('To-Do', self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
 		#self.fail('Finish Test!')
 
 
