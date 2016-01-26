aeb582b138e83a6d4f473fba88cdfc221d0887c8
Michael <michael.loukeris@icloud.com>
2016-01-26 16:32:17 +0200

    FT ready

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -31,6 +31,12 @@ class NewVisitorTest(LiveServerTestCase):
 		inputbox.send_keys('Buy peacock feathers')
 		inputbox.send_keys(Keys.ENTER)
 
+		edith_list_url = self.browser.current_url
+		# check redirection to new url
+		self.assertRegexpMatches(edith_list_url, '/lists/.+')
+
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+
 		# insert second value
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly')
