b4db3d1cb0528b4caf413fe914d12ccb7a401f20
Michael <michael.loukeris@icloud.com>
2016-01-26 12:01:05 +0200

    Chapter 5 function test

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	# page title and header
 	def test_check_title(self):
 		self.browser.get('http://localhost:8000')
@@ -18,7 +23,7 @@ class NewVisitorTest(unittest.TestCase):
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do', header_text)
 
-		# input box 
+		# input box
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
@@ -26,13 +31,14 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox.send_keys('Buy peacock feathers')
 		inputbox.send_keys(Keys.ENTER)
 
-		# table
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_element_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1:Buy peacock feathers' for row in rows), 
-			'New to-do item did not appear in table')
-		#self.fail('Finish Test!')
+		# insert second value
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+
+		# check values
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 
 
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -4,4 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home_page(request):
-	return render(request, 'home.html', {})
+	item = request.POST.get('item_text', '')
+	context = {
+		'new_item_text': item
+	}
+	return render(request, 'home.html',context)
