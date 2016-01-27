3030cdefdc3603bb9ace65104795aedbde87b7c1
Michael <michael.loukeris@icloud.com>
2016-01-27 10:36:13 +0200

    URLS

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -15,31 +15,18 @@ class HomePageTest(TestCase):
 		response = self.client.get('/')
 		self.assertTemplateUsed(response, 'home.html')
 
-	def test_home_page_can_save_a_POST_request(self):
-		request = HttpRequest()
-		request.method = 'POST'
-		request.POST['item_text'] = 'A new list item'
-		response = home_page(request)
 
+class NewListTest(TestCase):
+	# POST SAVE
+	def test_saving_a_POST_request(self):
+		self.client.post('/lists/new', data={'item_text': 'A new list item'})
 		self.assertEqual(Item.objects.count(), 1)
-		first_item = Item.objects.first()
-		self.assertEqual('A new list item', first_item.text)
-		# expected_html = render_to_string('home.html',
-		# {'new_item_text': 'A new list item'}, request=request)
-		# self.assertEqual(response.content.decode(), expected_html)
-
-	def test_home_page_redirects_after_POST(self):
-		request = HttpRequest()
-		request.method = 'POST'
-		request.POST['item_text'] = 'A new list item'
-		response = home_page(request)
-		self.assertEqual(response.status_code, 302)
-		self.assertEqual(response['location'], '/lists/the-only-list')
-
-	def test_home_page_only_saves_items_when_necessary(self):
-		request = HttpRequest()
-		home_page(request)
-		self.assertEqual(Item.objects.count(), 0)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new list item')
+	# POST REDIRECT
+	def test_redirects_after_POST(self):
+		response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+		self.assertRedirects(response, '/lists/the-only-list/')
 
 
 class ItemModelTest(TestCase):
@@ -58,6 +45,7 @@ class ItemModelTest(TestCase):
 
 
 class LiveViewTest(TestCase):
+	# GET items
 	def test_displays_all_items(self):
 		Item.objects.create(text='item 1')
 		Item.objects.create(text='item 2')
@@ -65,6 +53,7 @@ class LiveViewTest(TestCase):
 		self.assertContains(response, 'item 1')
 		self.assertContains(response, 'item 2')
 
+	# GET template
 	def test_users_list_template(self):
 		response = self.client.get('/lists/the-only-list/')
 		self.assertTemplateUsed(response, 'list.html')
