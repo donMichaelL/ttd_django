0eb7f5429d2e245fd5ba9233200363985d5ea469
Michael <michael.loukeris@icloud.com>
2016-01-31 12:50:41 +0200

    User Bootstrap to improve layout

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from lists.views import home_page
 
@@ -22,3 +24,7 @@ urlpatterns = [
     url(r'^lists/', include('lists.urls')),
     url(r'^admin/', admin.site.urls),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
