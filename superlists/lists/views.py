a4fb243c75a3edcab8ca6840091f973507dd8427
Michael <michael.loukeris@icloud.com>
2016-01-31 11:45:05 +0200

    Views adding comments

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -7,19 +7,19 @@ from .models import Item, List
 def home_page(request):
 	return render(request, 'home.html')
 
-
+# get /lists
 def view_list(request, list_id):
 	my_list = List.objects.get(id=list_id)
 	#items = Item.objects.filter(list=my_list)
 	return render(request, 'list.html', {'list': my_list})
 
-
+# post /lists/new
 def new_list(request):
 	new_list = List.objects.create()
 	Item.objects.create(text=request.POST['item_text'], list=new_list)
 	return redirect('/lists/%d/' % (new_list.id,))
 
-
+# post /lists/add_item
 def add_item(request, list_id):
 	my_list = List.objects.get(id=list_id)
 	Item.objects.create(text=request.POST['item_text'], list=my_list)
